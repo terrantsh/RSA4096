31892738e0f033ae978afe7f27b2d6d276d7a4a7
tanshanhe <tanshanhe@foxmail.com>
2018-10-08 10:10:29 +0800

    Update rsa.c

--- RSA_4096_origin_public/rsa.c
+++ RSA_4096_origin_public/rsa.c
@@ -11,17 +11,16 @@ Description : RSA encrypted function
 #include "rsa.h"
 #include "bignum.h"
 
+static int public_block_operation(uint8_t *out, uint32_t *out_len, uint8_t *in, uint32_t in_len, rsa_pk_t *pk);
 
 void generate_rand(uint8_t *block, uint32_t block_len)
 {
     uint32_t i;
     for(i=0; i<block_len; i++) {
-        srand ((unsigned)time(NULL));
+        srand ((unsigned)time(NULL));   // real rand message
         block[i] = rand();
     }
 }
-
-static int public_block_operation(uint8_t *out, uint32_t *out_len, uint8_t *in, uint32_t in_len, rsa_pk_t *pk);
 
 int rsa_public_encrypt(uint8_t *out, uint32_t *out_len, uint8_t *in, uint32_t in_len, rsa_pk_t *pk)
 {
@@ -42,12 +41,10 @@ int rsa_public_encrypt(uint8_t *out, uint32_t *out_len, uint8_t *in, uint32_t in
         } while(byte == 0);
         pkcs_block[i] = byte;
     }
-
     pkcs_block[i++] = 0;
 
     memcpy((uint8_t *)&pkcs_block[i], (uint8_t *)in, in_len);
     status = public_block_operation(out, out_len, pkcs_block, modulus_len, pk);
-
     // Clear potentially sensitive information
     byte = 0;
     memset((uint8_t *)pkcs_block, 0, sizeof(pkcs_block));
